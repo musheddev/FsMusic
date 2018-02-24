a5c22500af6f3af66cb96b71fc341c90c1e6f487
musheddev <orlandotheegg@gmail.com>
2018-02-24 14:32:37 -0700

    Fix Readme and rungeKutta method

--- FsMusic/NLDynamics.fs
+++ FsMusic/NLDynamics.fs
@@ -9,7 +9,7 @@ let rungeKutta (fn :D->D) (x :D) (dt :D) = //fn x -> dx/dt
     let k2 = fn(x + (D 0.5)*k1)*dt
     let k3 = fn(x + (D 0.5)*k2)*dt
     let k4 = fn(x + k3)*dt
-    x + (1/6)*(k1 + k2 + k3 + k4)
+    x + (1./6.)*(k1 + (D 2.)*k2 + (D 2.)*k3 + k4)
 
 
 module Tests =
@@ -17,8 +17,10 @@ module Tests =
     let test1() =
         let fn (x :D) = x*(1-x)
         let dt = 0.1
-        let chart = List.mapFold (fun (x :D) t -> 
+        let chart = 
+            List.mapFold (fun (x :D) t -> 
                 let xn = rungeKutta fn x (D dt)
-                ((t, float xn),xn)) (D 2.) [for t in 0. .. dt .. 10. -> t]
+                ((t, float xn),xn)) (D 1.5) [for t in 0. .. dt .. 10. -> t]
         printfn "%O" (fst chart)        
         Chart.Line(chart |> fst).ShowChart()
+    test1() |> ignore
--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 ## Composing Music
 This project aims to provide functional interface and data structures for composing music.
 
-
-## Non-liear Dynamics
+## Non-linear Dynamics
 This project also aims to provide non-liear modeling of music instrutments and tools to capture many of the non-linear interactions in sound rendering.
 The aliens folder contains some of the papers I am exploring. 
 
 ## Automatic Compositon
-The above two compoents to FsMusic will be used to explore automatic music composition.
+The above two compoents will be used to explore automatic music composition.
