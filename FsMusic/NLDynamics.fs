0645481ae0a9253856185b7d283379bc0119e6fe
Orlando <orlandotheegg@gmail.com>
2018-03-28 16:33:31 -0600

    preliminary mode locking

--- FsMusic/NLDynamics.fs
+++ FsMusic/NLDynamics.fs
@@ -11,6 +11,23 @@ let rungeKutta (fn :D->D) (x :D) (dt :D) = //fn x -> dx/dt
     let k4 = fn(x + k3)*dt
     x + (1./6.)*(k1 + (D 2.)*k2 + (D 2.)*k3 + k4)
 
+let wave (amp :D) (w :D) (phase :D) (t :D) =
+    amp * sin(w*t + phase)
+
+//mode locking equations from Fletcher 1999
+
+let adot (g :(D->D)->D) (a :D) (amp :D) (w :D) (phase :D) (yn :D->D) (t :D) =
+    g(yn) * cos(w*t + phase) / w - a*amp/2
+
+let fdot (g :(D->D)->D) (amp :D) (w :D) (phase :D) (yn :D->D) (t :D) =
+    - g(yn) * sin(w + phase) / (w*amp)
+
+let makeg (c1 :D) (c2 :D) (p :D) (q :D) = //linear case but summation doesn't make sense  
+    fun (yfn :D->D) ->
+         let y fn = List.sumBy (fun x -> fn(D x)) [0.0 .. 0.01 .. 6.28]
+         c1*(y yfn) + c2*(y (diff yfn))
+
+
 
 module Tests =
     open FSharp.Charting
